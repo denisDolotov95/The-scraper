82ef9b90bdf18ec500f1f336e55be61555240fed
fox <gambit0095.mail@gmail.com>
2026-04-26 17:59:41 +0300

    update database

--- app/database/model.py
+++ app/database/model.py
@@ -9,9 +9,9 @@ class Base(DeclarativeBase):
     pass
 
 
-class INN(Base):
+class Fedresurs(Base):
 
-    __tablename__ = "inn"
+    __tablename__ = "fedresurs"
 
     __table_args__ = (
         UniqueConstraint("inn_number", "last_date", name="uq_inn_numb_last_date"),
@@ -26,7 +26,6 @@ class INN(Base):
     inn_number: Mapped[str] = mapped_column(nullable=False)
     case_number: Mapped[str] = mapped_column(nullable=False)
     last_date: Mapped[date] = mapped_column(nullable=False)
-    name_document: Mapped[str] = mapped_column(nullable=True)
     # created_at: Mapped[datetime] = mapped_column(default=datetime.now)
 
     def __init__(
@@ -34,18 +33,60 @@ class INN(Base):
         inn_number: str,
         case_number: str,
         last_date: date,
-        name_document: str = None,
     ):
 
         self.inn_number = inn_number
+        self.case_number = case_number
+        self.last_date = last_date
+
+    def __repr__(self):
+
+        return f"Fedresurs(id={self.id}, inn_number={self.inn_number}, case_number={self.case_number}, last_date={self.last_date})"
+
+    def __str__(self):
+
+        return f"id={self.id}, inn_number={self.inn_number}, case_number={self.case_number}, last_date={self.last_date}"
+
+
+class KadArbitr(Base):
+
+    __tablename__ = "kadarbitr"
+
+    __table_args__ = (
+        UniqueConstraint(
+            "case_number",
+            "last_date",
+            "name_document",
+            name="uq_case_numer_last_date_name_document",
+        ),
+    )
+
+    kadarbitr_id_seq = Sequence("kadarbitr_id_seq", start=0, increment=1)
+
+    # id: Mapped[int] = mapped_column(
+    #     inn_id_seq, server_default=inn_id_seq.next_value(), primary_key=True
+    # )
+    id: Mapped[int] = mapped_column(kadarbitr_id_seq, primary_key=True)
+    case_number: Mapped[str] = mapped_column(nullable=False)
+    last_date: Mapped[date] = mapped_column(nullable=False)
+    name_document: Mapped[str] = mapped_column(nullable=False)
+    # created_at: Mapped[datetime] = mapped_column(default=datetime.now)
+
+    def __init__(
+        self,
+        case_number: str,
+        last_date: date,
+        name_document: str,
+    ):
+
         self.case_number = case_number
         self.last_date = last_date
         self.name_document = name_document
 
     def __repr__(self):
 
-        return f"INN(id={self.id}, inn_number={self.inn_number}, case_number={self.case_number}, last_date={self.last_date})"
+        return f"KadArbitr(id={self.id}, case_number={self.case_number}, last_date={self.last_date}, name_document={self.name_document})"
 
     def __str__(self):
 
-        return f"id={self.id}, inn_number={self.inn_number}, case_number={self.case_number}, last_date={self.last_date}"
+        return f"id={self.id}, case_number={self.case_number}, last_date={self.last_date}, name_document={self.name_document}"
